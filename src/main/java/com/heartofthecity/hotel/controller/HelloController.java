e0fe088fe9f5b33aa46a21538252a576d35a67d8
Ahmet Kenan Yalcin <kenlelasya@gmail.com>
2025-09-17 08:09:33 +0200

    V2

--- src/main/java/com/heartofthecity/hotel/controller/HelloController.java
+++ src/main/java/com/heartofthecity/hotel/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.heartofthecity.hotel.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Merhaba Dünya! Heart of The City Hotel uygulamasına hoş geldiniz.";
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/greeting")
+    public String getGreeting(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+        return messageSource.getMessage("greeting", null, locale);
     }
 }
--- src/test/java/com/heartofthecity/hotel/HotelApplicationTests.java
+++ src/test/java/com/heartofthecity/hotel/HotelApplicationTests.java
@@ -11,3 +11,41 @@ class HotelApplicationTests {
 	}
 
 }
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        // Dosya ismi, extension olmadan 'messages'
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.forLanguageTag("tr"));
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+}
+
